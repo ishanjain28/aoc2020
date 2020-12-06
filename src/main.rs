c2309defe84f4a5f4d3b9c7c448d336ff9e7dc8e
Ishan Jain <contact@ishanjain.me>
2020-12-06 10:40:31 +0530

    Added Day 6 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,25 @@
-use std::collections::HashSet;
 use std::io::{stdin, Read};
 
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let filled_seats = input
-        .split('\n')
+    let questions = input
+        .split("\n\n")
         .filter(|x| !x.is_empty())
         .into_iter()
-        .map(|seat| {
-            let seat = seat.chars();
-            let mut region: u16 = 0;
+        .map(|set| {
+            let mut q = [false; 26];
 
-            for s in seat {
-                region <<= 1;
-                region |= match s {
-                    'B' | 'R' => 1,
-                    'F' | 'L' | _ => 0,
-                };
-            }
+            set.chars()
+                .filter(|&x| char::is_alphabetic(x))
+                .for_each(|y| {
+                    q[(y as u8 - 97) as usize] = true;
+                });
 
-            (region >> 3) * 8 + (region & 0b111)
+            q.iter().fold(0, |a, &x| if x { a + 1 } else { a })
         })
-        .collect::<HashSet<u16>>();
+        .sum::<i32>();
 
-    let min = *filled_seats.iter().min().unwrap();
-    let max = *filled_seats.iter().max().unwrap();
-
-    for i in min..max {
-        if !filled_seats.contains(&i) {
-            println!("seat = {}", i);
-            break;
-        }
-    }
+    println!("questions = {:?}", questions);
 }
